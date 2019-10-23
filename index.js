2dc9ae8bd85784939a9555dc7965797a848e6fc6
Dylan Awalt-Conley <web@dylan.ac>
2019-10-23 13:43:53 -0400

    replaced depricated new Buffer() with Buffer.from()

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ AWS.config.update({ region: 'us-east-1' })
 module.exports = encrypted => new Promise((resolve, reject) => {
     console.log('decrypting key...')
     const kms = new AWS.KMS()
-    kms.decrypt({ CiphertextBlob: new Buffer(encrypted, 'base64') }, (err, data) => {
+    kms.decrypt({ CiphertextBlob: Buffer.from(encrypted, 'base64') }, (err, data) => {
         if (err) {
             reject(err)
         } else if (data === null) {
