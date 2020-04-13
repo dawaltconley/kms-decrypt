3dafbda17e5b3e166d3e18ca7a6c6cf7f96e4d99
Dylan Awalt-Conley <web@dylan.ac>
2020-04-12 23:15:01 -0400

    separated into encrypt and decrypt methods, fixed error handling so that it throws errors when recieved

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const AWS = require('aws-sdk')
+const kms = new AWS.KMS()
 
-module.exports = (encrypted, opts={}) => new Promise((resolve, reject) => {
-    if (opts.region)
-        AWS.config.update({ region: opts.region })
-    const kms = new AWS.KMS()
-    kms.decrypt({ CiphertextBlob: Buffer.from(encrypted, 'base64') }, (err, data) => {
-        if (err) {
-            reject(err)
-        } else if (data === null) {
-            reject(new Error('Unable to decrypt key, KMS returned null. This is likely due to a lack of decryption permissions.'))
-        }
-        resolve(data.Plaintext.toString('ascii'))
-    })
+exports.encrypt = (params, { region }={}) => new Promise((resolve, reject) => {
+    if (region)
+        AWS.config.update({ region: region })
+    kms.encrypt(params, (err, data) =>
+        err ? reject(err) : resolve(data.CiphertextBlob.toString('ascii')))
+})
+
+exports.decrypt = (encrypted, { region }={}) => new Promise((resolve, reject) => {
+    if (region)
+        AWS.config.update({ region: region })
+    kms.decrypt({ CiphertextBlob: Buffer.from(encrypted, 'base64') }, (err, data) =>
+        err ? reject(err) : resolve(data.Plaintext.toString('ascii')))
 })
